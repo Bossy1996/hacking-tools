8090421e99f287d2641deea2feb42c3cb139ea41
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-03-27 18:47:00 +0100

    input exceptions added

--- scanning/portscan1.py
+++ scanning/portscan1.py
@@ -3,9 +3,14 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP PACKAGE
-
-host = "192.168.0.1"
-port = 443
+try:   
+    host = str(input("[*] Enter the host to scan: "))
+except ValueError:
+    print("Value Error!")
+try:
+    port = int(input("[*] Enter the port to scan: "))
+except ValueError:
+    print("Value Error, it must be an integer!")
 
 def portscanner(port):
     if sock.connect_ex((host, port)):
