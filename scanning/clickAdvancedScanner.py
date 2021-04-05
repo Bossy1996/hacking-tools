5fbaf839f5e5ba36d9fc6f4924ac0614751cb68d
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-04-05 15:43:26 +0200

    things

--- scanning/clickAdvancedScanner.py
+++ scanning/clickAdvancedScanner.py
@@ -6,13 +6,22 @@ import click
 import sys
 
 
+def portScan(tgtHost, tgtPort):
+    try:
+        tgtIp = gethostbyname(tgtHost)
+    except:
+        print("Unknown host %s" %tgtHost)
+
+    try:
+        tgtName = gethostbyaddr(tgtIp)
+
+
 @click.command()
 @click.option('--host', required=True ,type=str, help='specify target host.',)
 @click.option('--port', type=str, help='specify target ports separated by comas.')
 def main(host, port):
     tgtHost = host
     tgtPort = port.split(',')
-
     portScan(tgtHost, tgtPort)
 
 if __name__ == '__main__':
